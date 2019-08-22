bda595b0bf161abff17a7e27d82719b92dd9aebe
Vasyl Batig <ziki_007@ukr.net>
2019-08-22 22:45:19 +0300

    change js

--- js/main.js
+++ js/main.js
@@ -2,17 +2,32 @@ function thisFileUpload() {
     document.getElementById("file").click();
 };
 
+// var control = document.getElementById("file");
+// control.addEventListener("change", function(event) {
+//     var i = 0,
+//         files = control.files,
+//         len = files.length;
+ 
+//     for (; i < len; i++) {
+//         console.log("Filename: " + files[i].name);
+//         console.log("Type: " + files[i].type);
+//         console.log("Size: " + files[i].size + " bytes");
+//     }
+ 
+// }, false);
 var control = document.getElementById("file");
-control.addEventListener("change", function(event) {
-    var i = 0,
-        files = control.files,
-        len = files.length;
- 
-    for (; i < len; i++) {
-        console.log("Filename: " + files[i].name);
-        console.log("Type: " + files[i].type);
-        console.log("Size: " + files[i].size + " bytes");
-        console.log("Size: " + files[i].date.getTime);
-    }
- 
-}, false);
+control.addEventListener("change", function(e){
+    var fileName = e.target.files[0].name;
+    var type = e.target.files[0].type;
+    var size = e.target.files[0].size;
+    
+    console.log(fileName, type, size);
+})
+
+// let newFile = {
+//     name: fileName,
+//     type: type,
+//     size: size
+// }
+
+// console.log(newFile);
